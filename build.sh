26e69afd541171ed45bdf436a3ddb29c9635045c
Silence <chenmohaha_2000@163.com>
2018-04-08 15:18:27 +0800

    打镜像

--- build.sh
+++ build.sh
@@ -13,6 +13,12 @@ tag_name=mvc:latest
 
 docker build -f ./Dockerfile -t ${tag_name} .
 
+docker login -u chenmohaha_2000@163.com -p cm13811666786 hub.c.163.com
+
+docker tag mvc:latest hub.c.163.com/keysilence/store/docker/mvc:1.0.0
+
+docker push hub.c.163.com/keysilence/store/docker/mvc:1.0.0
+
 #echo "docker login -u ggov ${base_url}"
 #docker login -u ggov -p !qaz2wsX ${base_url}
 #
