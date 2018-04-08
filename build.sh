232077a3a8484c16c12cf5ef8624d9d0dcaa94c4
Silence <chenmohaha_2000@163.com>
2018-04-08 16:46:30 +0800

    打镜像

--- build.sh
+++ build.sh
@@ -1,30 +1,33 @@
 #!/bin/sh
 
+echo "############## start ##############"
+
+# 拉取代码
 git pull
 
-echo "############## start ##############"
+# 编译
 mvn clean install -Dmaven.test.skip=true
 
-cp mvc/target/mvc.war ./
+# 拷贝新编译的mvc.war
+cp -f mvc/target/mvc.war ./
 
-#base_url=10.19.248.200:30100
-#docker_url=${base_url}/g_laikang/laikang-app-admin:latest
+# 删除本地已打镜像mvc:latest
 docker rmi -f mvc:latest
 
+# 设置镜像标签名
 tag_name=mvc:latest
 
+# 构建镜像
 docker build -f ./Dockerfile -t ${tag_name} .
 
+# 登录网易云镜像服务器
 docker login -u chenmohaha_2000@163.com -p cm13811666786 hub.c.163.com
 
+# 修改标签
 docker tag mvc:latest hub.c.163.com/keysilence/store/docker/mvc:1.0.0
 
+# 推到网易云
 docker push hub.c.163.com/keysilence/store/docker/mvc:1.0.0
-
-#echo "docker login -u ggov ${base_url}"
-#docker login -u ggov -p !qaz2wsX ${base_url}
-#
-#docker push ${docker_url}
 
 #git clean -df
 
