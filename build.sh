482b59451ff53a6f9bf3eb4e03a9d8c1660dd272
Silence <chenmohaha_2000@163.com>
2018-04-08 14:52:14 +0800

    MVC汇总

--- build.sh
+++ build.sh
@@ -9,9 +9,9 @@ cp mvc/target/mvc.war ./
 
 #base_url=10.19.248.200:30100
 #docker_url=${base_url}/g_laikang/laikang-app-admin:latest
-#tag_name=mvc:latest
+tag_name=mvc:latest
 
-#docker build -f ./Dockerfile -t ${tag_name} .
+docker build -f ./Dockerfile -t ${tag_name} .
 
 #echo "docker login -u ggov ${base_url}"
 #docker login -u ggov -p !qaz2wsX ${base_url}
